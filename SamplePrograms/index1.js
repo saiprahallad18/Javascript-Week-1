7b454a3e331e9f8a5a3189c7b28647cdd1a34929
saiprahallad18 <saiprahallad18@gmail.com>
2026-01-22 15:20:28 +0530

    Add JS day 1 programs

---
+++ SamplePrograms/index1.js
@@ -0,0 +1,122 @@
+// let k = 9;
+// if(k%2==0){
+//     console.log("Number is even");
+// }
+// else{
+//     console.log("number is odd");
+// }
+
+// k%2==0?console.log("mun is even"):console.log("num is odd");
+
+   
+// for(i=0;i<100;i++){
+//     console.log(i);
+//     if(i==10){
+//         break;
+//     }
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     if(i==5){
+//         break
+//     }
+//     i++ 
+// }
+
+
+
+
+// function AddNumbers(a,b){
+//     let sum=a+b;
+//     return sum;
+// }
+// let sum=AddNumbers(10,20);
+// console.log(`Sum ${sum}`); 
+
+
+// let number = 9;
+// if(number>0){
+//     console.log("positive");
+// }
+// else if(number<0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("undefined");
+// }
+
+
+
+// let dayNumber = 7;
+// switch(dayNumber){
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+//     case 5:
+//         console.log("Thursday");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+
+// let value = 0;
+// if (value){
+//     console.log("Truthy");
+// }
+// else{
+//     console.log("falsy");
+// }
+
+
+let marks = 10;
+if(marks>= 90){
+    console.log("A")
+}
+else if(marks >= 75){
+    console.log("B")
+}
+else if (marks>=50){
+    console.log("C")
+}
+else{
+    console.log("Fail")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
